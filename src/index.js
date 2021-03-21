650b8757d2140016eb5193975aed8efd28fc7cb6
HajarAlhassan <79155491+HajarAlhassan@users.noreply.github.com>
2021-03-21 13:20:36 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Routes from './components/Routes'
-//import Game from './components/tic-tac-toc/Game'
-// import MyRouter from './components/MyRouter'
-// import MovieApp from './components/myMovies/MovieApp'
+
 import Router from "./components/MyRouter"
 ReactDOM.render(<Router/>, document.getElementById('root'))
